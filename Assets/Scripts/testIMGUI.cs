36e6bb75109a49df7a6a2de1f2a8613bc6146bab
YgritteSnow <773963715@qq.com>
2017-05-19 07:08:47 +0800

    存点图片素材

---
+++ Assets/Scripts/testIMGUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testIMGUI : MonoBehaviour {
+    public Texture tex;
+    public GUIStyle style;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnGUI()
+    {
+        // This line feeds "This is the tooltip" into GUI.tooltip
+        GUI.Button(new Rect(10, 10, 100, 20), new GUIContent("1111", tex, "This is the tooltip 111"), style);
+        GUI.Label(new Rect(10, 40, 100, 20), GUI.tooltip);
+
+        GUI.Button(new Rect(10, 40, 100, 20), new GUIContent("2222", tex, "This is the tooltip 222"), style);
+        GUI.Label(new Rect(10, 70, 100, 20), GUI.tooltip);
+    }
+}
