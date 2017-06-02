53fd4d87320c57c3e59cb5cf5cc66cdca2d1779f
YgritteSnow <773963715@qq.com>
2017-06-02 08:28:58 +0800

    lol

--- Assets/Scripts/Mirror/JMirrorTransformY.cs
+++ Assets/Scripts/Mirror/JMirrorTransformY.cs
@@ -3,20 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Transform))]
-public class JMirrorTransformY : MonoBehaviour {
+public class JMirrorTransformY : MonoBehaviour
+{
 	
 	private Transform m_trans;
 	// Use this for initialization
 	void Start () {
 		m_trans = GetComponent<Transform>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			OnMirror();
-		}
+
+		gameObject.tag = JTmpTagManager.Instance.GetTagName(this);
 	}
 
 	void OnMirror()
